e0d3a7e89230093d887ff5696c4d05c4f804dc80
srikanth.bhompally33@gmail.com <surinenipurnachander@gmail.com>
2023-09-27 15:52:13 -0400

    changed test file, modified requirements.txt file, and added visualize.py file for visualizing data automatically

--- transform.py
+++ transform.py
@@ -3,7 +3,9 @@ import pandas as pd
 df = pd.read_csv('extracted_data.csv')
 
 # Perform data transformation (example: filter data where 'column_name' equals 'some_value')
-filtered_df = df[df['value'] >= 40.0]
+product_names_of_interest = ['TV', 'Earphones', 'Laptops']
+
+filtered_df = df[(df['year'] >= 2015) & (df['year'] <= 2020) & (df['value'] >= 56)]
 
 filtered_df.to_csv('transformed_data.csv', index=False)
 
